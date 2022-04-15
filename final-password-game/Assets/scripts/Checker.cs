5d00d9cf76f1c46e589b6094e66f042ddce269a6
Rhalith <akmannuhyigit@gmail.com>
2022-04-16 02:20:47 +0300

    checker script added

---
+++ final-password-game/Assets/scripts/Checker.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Checker : MonoBehaviour
+{   
+    public GameObject number1, number2, number3, number4;
+
+    void Start()
+    {
+            
+    }
+
+    void Update()
+    {
+        
+    }
+}
