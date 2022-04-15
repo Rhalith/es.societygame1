44f6ffb02219b62c0a7ce2648eff886c29a53730
Rhalith <akmannuhyigit@gmail.com>
2022-04-16 02:47:12 +0300

    numbercontroller changed

--- final-password-game/Assets/Scripts/NumberController.cs
+++ final-password-game/Assets/Scripts/NumberController.cs
@@ -8,8 +8,15 @@ public class NumberController : MonoBehaviour
 {
     public Button upButton;
     public Button downButton;
-    public TextMeshPro text;
+    public TMP_Text text;
     public int currentNumber = 0;
+    public int whatIAm;
+    Checker checker;
+    public GameObject password;
+    private void Start()
+    {
+        checker = password.GetComponent<Checker>();
+    }
     public void AddNumber()
     {
         if (currentNumber == 9)
@@ -39,6 +46,29 @@ public class NumberController : MonoBehaviour
     public int GetCurrent()
     {
         return currentNumber;
+    }
+    public void CheckNumber()
+    {
+        switch (whatIAm)
+        {
+            case 1: 
+                if (currentNumber == checker.realnum2 || currentNumber == checker.realnum3 || currentNumber == checker.realnum4)
+                {
+                    checker.statusNum1 = "yellow";
+                }
+                else if (currentNumber == checker.realnum1)
+                {
+                    checker.statusNum1 = "green";
+                }
+                break;
+            case 2:
+                currentNumber--;
+                break;
+            default:
+                currentNumber++;
+                break;
+
+        }
     }
 }
 
--- final-password-game/Assets/scripts/Checker.cs
+++ final-password-game/Assets/scripts/Checker.cs
@@ -7,9 +7,13 @@ public class Checker : MonoBehaviour
 {   
     public GameObject number1, number2, number3, number4;
 
+    public int realnum1,realnum2,realnum3,realnum4;
+    public string statusNum1, statusNum2, statusNum3, statusNum4;
+    
+    NumberController numberController;
     void Start()
     {
-            
+        numberController = number1.GetComponent<NumberController>();
     }
 
     void Update()
