cb327e61210f608beda8ea186c27e67cf8c7118d
Rhalith <akmannuhyigit@gmail.com>
2022-04-16 16:33:38 +0300

    test bölümü ev olarak dizayn edildi

--- final-password-game/Assets/scripts/Checker.cs
+++ final-password-game/Assets/scripts/Checker.cs
@@ -12,6 +12,8 @@ public class Checker : MonoBehaviour
     public string REMEMBER_REAL = "NUMBER_5OR6";
     private int curNum1, curNum2, curNum3, curNum4, curNum5, curNum6;
     private bool isNum1, isNum2, isNum3, isNum4, isNum5, isNum6;
+    public GameObject num1Color, num2Color, num3Color, num4Color, num5Color, num6Color;
+    public int lives; public TMP_Text text;
     public void setNum(int controlnum, int curnum)
     {
         if (controlnum == 1)
@@ -33,145 +35,177 @@ public class Checker : MonoBehaviour
     }
     public void Submit()
     {
-        switch(CheckNumber(curNum1,realnum2) || CheckNumber(curNum1,realnum3) || CheckNumber(curNum1, realnum4) || CheckNumber(curNum1,realnum5) || CheckNumber(curNum1, realnum6))
+        lives--;
+        text.text = lives.ToString();
+        switch (lives)
         {
-            case true:
-                Debug.Log("yellow for 1");
+            case 0:
+                GameObject.Find("CM vcam1").SetActive(false);
+                GameObject.Find("CM vcam3").SetActive(false);
+                GameObject.Find("CM vcam4").SetActive(false);
+                text.text = 0.ToString();
                 break;
-            case false:
-                break;
-        }
-        switch (CheckNumber(curNum2, realnum1) || CheckNumber(curNum2, realnum3) || CheckNumber(curNum2, realnum4) || CheckNumber(curNum2, realnum5) || CheckNumber(curNum2, realnum6))
-        {
-            case true:
-                Debug.Log("yellow for 2");
-                break;
-            case false:
-                break;
-        }
-        switch (CheckNumber(curNum3, realnum2) || CheckNumber(curNum3, realnum1) || CheckNumber(curNum3, realnum4) || CheckNumber(curNum3, realnum5) || CheckNumber(curNum3, realnum6))
-        {
-            case true:
-                Debug.Log("yellow for 3");
-                break;
-            case false:
-                break;
-        }
-        switch (CheckNumber(curNum4, realnum2) || CheckNumber(curNum4, realnum3) || CheckNumber(curNum4, realnum1) || CheckNumber(curNum4, realnum5) || CheckNumber(curNum4, realnum6))
-        {
-            case true:
-                Debug.Log("yellow for 4");
-                break;
-            case false:
-                break;
-        }
-        switch (CheckNumber(curNum5, realnum2) || CheckNumber(curNum5, realnum3) || CheckNumber(curNum5, realnum1) || CheckNumber(curNum4, realnum5) || CheckNumber(curNum5, realnum6))
-        {
-            case true:
-                Debug.Log("yellow for 5");
-                break;
-            case false:
-                break;
-        }
-        switch (CheckNumber(curNum6, realnum2) || CheckNumber(curNum6, realnum3) || CheckNumber(curNum6, realnum1) || CheckNumber(curNum6, realnum5) || CheckNumber(curNum4, realnum6))
-        {
-            case true:
-                Debug.Log("yellow for 6");
-                break;
-            case false:
-                break;
-        }
-        switch (CheckNumber(curNum1, realnum1))
-        {
-            case true:
-                Debug.Log("green for 1");
-                isNum1 = true;
-                break;
-            case false:
-                break;
-        }
-        switch (CheckNumber(curNum2, realnum2))
-        {
-            case true:
-                Debug.Log("green for 2");
-                isNum2 = true;
-                break;
-            case false:
-                break;
-        }
-        switch (CheckNumber(curNum3, realnum3))
-        {
-            case true:
-                Debug.Log("green for 3");
-                isNum3 = true;
-                break;
-            case false:
-                break;
-        }
-        switch (CheckNumber(curNum4, realnum4))
-        {
-            case true:
-                Debug.Log("green for 4");
-                isNum4 = true;
-                break;
-            case false:
-                break;
-        }
-        switch (CheckNumber(curNum5, realnum5))
-        {
-            case true:
-                Debug.Log("green for 5");
-                isNum5 = true;
-                break;
-            case false:
-                break;
-        }
-        switch (CheckNumber(curNum6, realnum6))
-        {
-            case true:
-                Debug.Log("green for 6");
-                isNum6 = true;
-                break;
-            case false:
-                break;
-        }
-        switch (CheckAllNumbers())
-        {
-            case true:
-                
-                switch (WhatIReference)
+            default:
+                switch (CheckNumber(curNum1, realnum2) || CheckNumber(curNum1, realnum3) || CheckNumber(curNum1, realnum4) || CheckNumber(curNum1, realnum5) || CheckNumber(curNum1, realnum6))
                 {
-                    case "Z":
-                        PlayerPrefs.SetString("letter1", "Z");
+                    case true:
+                        Debug.Log("yellow for 1");
+                        num1Color.GetComponent<Image>().color = Color.yellow;
                         break;
-                    case "O":
-                        PlayerPrefs.SetString("letter2", "O");
+                    case false:
+                        num1Color.GetComponent<Image>().color = Color.white;
                         break;
-                    case "R":
-                        PlayerPrefs.SetString("letter3", "R");
-                        break;
-                    case "L":
-                        PlayerPrefs.SetString("letter4", "L");
-                        break;
-                    case "A":
-                        PlayerPrefs.SetString("letter5", "A");
-                        break;
-                    case "M":
-                        PlayerPrefs.SetString("letter6", "M");
-                        break;
-                    case "A2":
-                        PlayerPrefs.SetString("letter7", "A");
-                        break;
-                    case "K":
-                        PlayerPrefs.SetString("letter8", "K");
-                        break;
-                    
                 }
-                SceneManager.LoadScene("Sections");
-                break;
+                switch (CheckNumber(curNum2, realnum1) || CheckNumber(curNum2, realnum3) || CheckNumber(curNum2, realnum4) || CheckNumber(curNum2, realnum5) || CheckNumber(curNum2, realnum6))
+                {
+                    case true:
+                        Debug.Log("yellow for 2");
+                        num2Color.GetComponent<Image>().color = Color.yellow;
+                        break;
+                    case false:
+                        num2Color.GetComponent<Image>().color = Color.white;
+                        break;
+                }
+                switch (CheckNumber(curNum3, realnum2) || CheckNumber(curNum3, realnum1) || CheckNumber(curNum3, realnum4) || CheckNumber(curNum3, realnum5) || CheckNumber(curNum3, realnum6))
+                {
+                    case true:
+                        Debug.Log("yellow for 3");
+                        num3Color.GetComponent<Image>().color = Color.yellow;
+                        break;
+                    case false:
+                        num3Color.GetComponent<Image>().color = Color.white;
+                        break;
+                }
+                switch (CheckNumber(curNum4, realnum2) || CheckNumber(curNum4, realnum3) || CheckNumber(curNum4, realnum1) || CheckNumber(curNum4, realnum5) || CheckNumber(curNum4, realnum6))
+                {
+                    case true:
+                        Debug.Log("yellow for 4");
+                        num4Color.GetComponent<Image>().color = Color.yellow;
+                        break;
+                    case false:
+                        num4Color.GetComponent<Image>().color = Color.white;
+                        break;
+                }
+                switch (CheckNumber(curNum5, realnum2) || CheckNumber(curNum5, realnum3) || CheckNumber(curNum5, realnum1) || CheckNumber(curNum4, realnum5) || CheckNumber(curNum5, realnum6))
+                {
+                    case true:
+                        Debug.Log("yellow for 5");
+                        num5Color.GetComponent<Image>().color = Color.yellow;
+                        break;
+                    case false:
+                        num5Color.GetComponent<Image>().color = Color.white;
+                        break;
+                }
+                switch (CheckNumber(curNum6, realnum2) || CheckNumber(curNum6, realnum3) || CheckNumber(curNum6, realnum1) || CheckNumber(curNum6, realnum5) || CheckNumber(curNum4, realnum6))
+                {
+                    case true:
+                        Debug.Log("yellow for 6");
+                        num6Color.GetComponent<Image>().color = Color.yellow;
+                        break;
+                    case false:
+                        num6Color.GetComponent<Image>().color = Color.white;
+                        break;
+                }
+                switch (CheckNumber(curNum1, realnum1))
+                {
+                    case true:
+                        Debug.Log("green for 1");
+                        num1Color.GetComponent<Image>().color = Color.green;
+                        isNum1 = true;
+                        break;
+                    case false:
+                        break;
+                }
+                switch (CheckNumber(curNum2, realnum2))
+                {
+                    case true:
+                        Debug.Log("green for 2");
+                        num2Color.GetComponent<Image>().color = Color.green;
+                        isNum2 = true;
+                        break;
+                    case false:
+                        break;
+                }
+                switch (CheckNumber(curNum3, realnum3))
+                {
+                    case true:
+                        Debug.Log("green for 3");
+                        num3Color.GetComponent<Image>().color = Color.green;
+                        isNum3 = true;
+                        break;
+                    case false:
+                        break;
+                }
+                switch (CheckNumber(curNum4, realnum4))
+                {
+                    case true:
+                        Debug.Log("green for 4");
+                        num4Color.GetComponent<Image>().color = Color.green;
+                        isNum4 = true;
+                        break;
+                    case false:
+                        break;
+                }
+                switch (CheckNumber(curNum5, realnum5))
+                {
+                    case true:
+                        Debug.Log("green for 5");
+                        num5Color.GetComponent<Image>().color = Color.green;
+                        isNum5 = true;
+                        break;
+                    case false:
+                        break;
+                }
+                switch (CheckNumber(curNum6, realnum6))
+                {
+                    case true:
+                        Debug.Log("green for 6");
+                        num6Color.GetComponent<Image>().color = Color.white;
+                        isNum6 = true;
+                        break;
+                    case false:
+                        break;
+                }
+                switch (CheckAllNumbers())
+                {
+                    case true:
 
-                
+                        switch (WhatIReference)
+                        {
+                            case "Z":
+                                PlayerPrefs.SetString("letter1", "Z");
+                                break;
+                            case "O":
+                                PlayerPrefs.SetString("letter2", "O");
+                                break;
+                            case "R":
+                                PlayerPrefs.SetString("letter3", "R");
+                                break;
+                            case "L":
+                                PlayerPrefs.SetString("letter4", "L");
+                                break;
+                            case "A":
+                                PlayerPrefs.SetString("letter5", "A");
+                                break;
+                            case "M":
+                                PlayerPrefs.SetString("letter6", "M");
+                                break;
+                            case "A2":
+                                PlayerPrefs.SetString("letter7", "A");
+                                break;
+                            case "K":
+                                PlayerPrefs.SetString("letter8", "K");
+                                break;
+
+                        }
+                        SceneManager.LoadScene("Sections");
+                        break;
+
+
+                }
+                break;
         }
+        
     }
     public bool CheckNumber(int curnum, int realnum)
     {
