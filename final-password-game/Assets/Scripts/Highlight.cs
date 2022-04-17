f06fe1363807bfbaaa4847346e9c80cc5f41a370
Rhalith <akmannuhyigit@gmail.com>
2022-04-17 17:09:46 +0300

    BİTTİ(K)

--- final-password-game/Assets/Scripts/Highlight.cs
+++ final-password-game/Assets/Scripts/Highlight.cs
@@ -7,6 +7,7 @@ public class Highlight : MonoBehaviour
     private MeshRenderer meshRenderer;
     private Material currentMat ;
     public Material targetMat;
+    public bool isClicked;
     void Start()
     {
      meshRenderer=GetComponent<MeshRenderer>();
@@ -15,10 +16,20 @@ public class Highlight : MonoBehaviour
 
     private void OnMouseEnter()
     {
-        meshRenderer.material = targetMat;   }
+        if (!isClicked)
+        {
+            meshRenderer.material = targetMat;
+        }
+          
+    }
 
     private void OnMouseExit()
     {
+        meshRenderer.material = currentMat;
+    }
+    public void clickTo()
+    {
+        isClicked = true;
         meshRenderer.material = currentMat;
     }
 }
